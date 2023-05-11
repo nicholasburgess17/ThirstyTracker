c6e3faafb2f56435af0ee756cb87569a2ebf0495
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-05-11 15:04:34 -0600

    rename files, create navbar component and import to app

--- client/App.js
+++ client/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import NavBar from './NavBar';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! So epic</Text>
       <StatusBar style="auto" />
+      <NavBar />
     </View>
   );
 }
---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+const NavBar = () => {
+  return (
+    <View style={{ flexDirection: 'row', backgroundColor: '#f2f2f2', height: 60, alignItems: 'center', justifyContent: 'space-between', paddingHorizontal: 10 }}>
+      <TouchableOpacity>
+        <Text style={{ fontWeight: 'bold', fontSize: 18 }}>Home</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={{ fontWeight: 'bold', fontSize: 18 }}>Plants</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={{ fontWeight: 'bold', fontSize: 18 }}>Settings</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={{ fontWeight: 'bold', fontSize: 18 }}>Search</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default NavBar;
