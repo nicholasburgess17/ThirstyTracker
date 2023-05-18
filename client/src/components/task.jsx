8ca89acb6a6b5332227b2e3e42a8902c846d5887
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-05-18 11:18:05 -0600

    create task component

---
+++ client/src/components/task.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+
+// Assume you have a function to fetch user's todos from the database
+const fetchUserTodos = async () => {
+  // Fetch user's todos from the database
+  // Return an array of todos
+  // Example: return await fetch('api/user/todos').then(response => response.json());
+  return [
+    { id: 1, title: 'Task 1' },
+    { id: 2, title: 'Task 2' },
+    { id: 3, title: 'Task 3' },
+  ];
+};
+
+const Task = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    // Fetch user's todos when the component mounts
+    fetchUserTodos()
+      .then((todos) => setTodos(todos))
+      .catch((error) => console.error('Error fetching todos:', error));
+  }, []);
+
+  return (
+    <View>
+      {/* Check if the todos array is empty */}
+      {todos.length === 0 ? (
+        // Display a message if no todos are found
+        <Text>No todos found.</Text>
+      ) : (
+        // If todos are present, map through the array and render Todo components
+        todos.map((todo) => <Todo key={todo.id} title={todo.title} />)
+      )}
+    </View>
+  );
+};
+
+const Todo = ({ title }) => {
+  return (
+    <View>
+      {/* Display the title of the todo */}
+      <Text>{title}</Text>
+    </View>
+  );
+};
+
+export default Task;
---
+++ client/src/components/todo.jsx
@@ -0,0 +1,10 @@
+// this component will be a todo list for each plant, it will populate the task component
+
+const Todo = () => {
+
+    return (
+        
+    );
+}
+
+export default Todo;
