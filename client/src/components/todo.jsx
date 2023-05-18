c696d8123d07c1c54f1c5366f7a07872fba7f44a
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-05-18 11:20:53 -0600

    create todo component

--- client/src/components/todo.jsx
+++ client/src/components/todo.jsx
@@ -1,10 +1,30 @@
-// this component will be a todo list for each plant, it will populate the task component
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-const Todo = () => {
+const Todo = ({ id, title }) => {
+  const handlePress = () => {
+    // Handle the press event for a todo item
+    console.log(`Todo ${id} pressed.`);
+  };
 
-    return (
-        
-    );
-}
+  return (
+    <TouchableOpacity onPress={handlePress}>
+      <View style={styles.container}>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  title: {
+    fontSize: 16,
+  },
+});
 
 export default Todo;
