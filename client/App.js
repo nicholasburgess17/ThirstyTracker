3b80204c41ff964cd2dddaf8d240ace5e2428ec8
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-05-31 14:31:04 -0600

    import components and client to app.js

--- client/App.js
+++ client/App.js
@@ -3,7 +3,9 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import NavBar from './src/components/navbar'
 import Header from './src/components/header';
 import Task from './src/components/task';
-
+import Account from './src/components/account';
+import Login from './src/components/login'
+import {supabase} from './src/supabaseClient'
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
